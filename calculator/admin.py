6ae8f20eb39a727218e8b8cf2a7cd8ea13053e93
AdamBoley <adamboley92@gmail.com>
2023-01-06 17:45:53 +0000

    register viscometer model in admin.py

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Viscometer
 
-# Register your models here.
+
+@admin.register(Viscometer)
+class ViscometerAdmin(admin.ModelAdmin):
+    """
+    """
+    list_filter = (
+        'size',
+        'serial_number',
+        'type',
+    )
+
+    list_display = (
+        'size',
+        'serial_number',
+        'constant',
+        'type',
+        'calibration_date',
+        'expiry_date',
+    )
+
+    search_fields = (
+        'size',
+        'serial_number',
+        'constant',
+        'type',
+    )
